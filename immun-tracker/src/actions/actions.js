462d505650e3eafe29214214ae24974bddf81f76
Ronald Libago <code@ronaldlibago.com>
2019-05-01 20:32:46 -0700

    Add apiCall to AddPatient
    Addpatient will now do a POST request to '/patients'
    when user submits new patient information

--- immun-tracker/src/actions/actions.js
+++ immun-tracker/src/actions/actions.js
@@ -1,28 +1,31 @@
 import axios from "axios";
+// LOG IN/OUT Actions
 export const LOGIN_START = "LOGIN_START";
 export const LOGIN_RESOLVED = "LOGIN_RESOLVED";
 export const LOGOUT = 'LOGOUT';
 
+// Globals
+const host = "https://infinite-castle-77802.herokuapp.com";
+
+// Action Creators
 export const login = creds => dispatch => {
-    dispatch({ type: LOGIN_START });
-  
-    return axios
-      .post("https://infinite-castle-77802.herokuapp.com/login", creds)
-      .then(res => {
-        localStorage.setItem("token", `Bearer ${res.data.token}`)
+  dispatch({ type: LOGIN_START });
 
-        console.log(res.data.payload)
-        dispatch({ type: LOGIN_RESOLVED });
-      })
-      .catch(err => {
-        console.log("login err: ", err);
-        if (err.response.status === 403) {
-          localStorage.removeItem("token");
-        }
-        dispatch({ type: LOGIN_RESOLVED });
-      });
-  };
+  return axios
+    .post(`${host}/login`, creds)
+    .then(res => {
+      localStorage.setItem("token", `Bearer ${res.data.token}`)
+
+      console.log(res.data.payload)
+      dispatch({ type: LOGIN_RESOLVED });
+    })
+    .catch(err => {
+      console.log("login err: ", err);
+      if (err.response.status === 403) {
+        localStorage.removeItem("token");
+      }
+      dispatch({ type: LOGIN_RESOLVED });
+    });
+};
 
 
-
- 
--- immun-tracker/src/components/UserViewPages/AddPatient/AddPatientContainer.js
+++ immun-tracker/src/components/UserViewPages/AddPatient/AddPatientContainer.js
@@ -1,50 +1,75 @@
 import React, { useState } from "react";
 // Components
 import DatePicker from "react-datepicker";
+// API Helper
+import { postPatient } from "./apiCalls";
 
 import "react-datepicker/dist/react-datepicker.css";
 
 export class AddPatientView extends React.Component {
-    state = {
-        firstName: "",
-        lastName: "",
-        birthDate: new Date(),
-    }
+  state = {
+    firstName: "",
+    lastName: "",
+    birthDate: new Date()
+  };
 
-    changeDate = date => {
-        this.setState({
-            birthDate: date
-        });
-    }
+  changeDate = date => {
+    this.setState({
+      birthDate: date
+    });
+  };
 
-    handleInputChange = event => {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    }
+  handleInputChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
 
-    render() {
-        const {firstName, lastName, birthDate } = this.state;
+  submitPatient = event => {
+    // Suppress default submit behavior
+    event.preventDefault();
 
-        return (
-            <div>
-                <input
-                    name="firstName"
-                    value={firstName}
-                    onChange={this.handleInputChange}
-                />
-                <input
-                    name="lastName"
-                    value={lastName}
-                    onChange={this.handleInputChange}
-                />
-                <DatePicker 
-                    selected={birthDate} // this works like the 'value' in input
-                    onChange={this.changeDate} // accepts onChange function which
-                    // would update state
-                    />
-            </div>
-        );
-    }    
+    const { firstName, lastName, birthDate } = this.state;
+
+    const newPatient = {
+      firstName,
+      lastName,
+      birthDate
+    };
+
+    postPatient(newPatient)
+      .then(() => {
+        console.log("success!");
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  };
+
+  render() {
+    const { firstName, lastName, birthDate } = this.state;
+
+    return (
+      <div>
+        <form onSubmit={this.submitPatient}>
+          <input
+            name="firstName"
+            value={firstName}
+            onChange={this.handleInputChange}
+          />
+          <input
+            name="lastName"
+            value={lastName}
+            onChange={this.handleInputChange}
+          />
+          <DatePicker
+            selected={birthDate} // this works like the 'value' in input
+            onChange={this.changeDate} // accepts onChange function which
+            // would update state
+          />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    );
+  }
 }
-
