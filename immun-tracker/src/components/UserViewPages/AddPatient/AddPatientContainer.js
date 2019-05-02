c942e05fc5d75d29e852bf9f8c9ee6ea13e9e42a
Ronald Libago <code@ronaldlibago.com>
2019-05-01 21:18:22 -0700

    Add check for empty fields
    Form will warn user if required fields are empty
    (firstName & lastName)

--- immun-tracker/src/components/UserViewPages/AddPatient/AddPatientContainer.js
+++ immun-tracker/src/components/UserViewPages/AddPatient/AddPatientContainer.js
@@ -10,7 +10,11 @@ export class AddPatientView extends React.Component {
   state = {
     firstName: "",
     lastName: "",
-    birthDate: new Date()
+    birthDate: new Date(),
+    blurred: {
+        firstName: false,
+        lastName: false
+    }
   };
 
   changeDate = date => {
@@ -46,23 +50,59 @@ export class AddPatientView extends React.Component {
       });
   };
 
+  isFieldFilled = fieldName => {
+    return Boolean(this.state[fieldName]);
+  };
+
+  showErrorMessage = fieldName => {
+      return this.state.blurred[fieldName] && !this.isFieldFilled(fieldName);
+  }
+
+  setBlur = (event) => {
+      // Reference for below:
+      //https://stackoverflow.com/a/44708693
+      const elementName = event.target.name;
+
+      const stateUpdater = (prevState) => ({
+          ...prevState,
+          blurred: {
+              ...prevState.blurred,
+              [elementName]: true
+          }
+      });
+
+      this.setState(stateUpdater);
+  }
+
   render() {
     const { firstName, lastName, birthDate } = this.state;
 
     return (
       <div>
         <form onSubmit={this.submitPatient}>
+          <label htmlFor="firstName">First Name</label>
           <input
             name="firstName"
             value={firstName}
             onChange={this.handleInputChange}
+            onBlur={this.setBlur}
           />
+          <div>
+            {this.showErrorMessage("firstName") ? "First Name is required!" : null}
+          </div>
+          <label htmlFor="lastName">Last Name</label>
           <input
             name="lastName"
             value={lastName}
             onChange={this.handleInputChange}
+            onBlur={this.setBlur}
           />
+          <div>
+            {this.showErrorMessage("lastName") ? "Last Name is required!" : null}
+          </div>
+          <label htmlFor="birthDate">Birth Date</label>
           <DatePicker
+            name="birthDate"
             selected={birthDate} // this works like the 'value' in input
             onChange={this.changeDate} // accepts onChange function which
             // would update state
