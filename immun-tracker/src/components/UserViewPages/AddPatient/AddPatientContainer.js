af377c06f78c7abc9d9b2885ad600c8b6d6a0ef8
Ronald Libago <code@ronaldlibago.com>
2019-05-01 19:52:16 -0700

    Modify how input state is handled. Add first/last name inputs.

--- immun-tracker/src/components/UserViewPages/AddPatient/AddPatientContainer.js
+++ immun-tracker/src/components/UserViewPages/AddPatient/AddPatientContainer.js
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { useState } from "react";
+// Components
+import DatePicker from "react-datepicker";
+
+import "react-datepicker/dist/react-datepicker.css";
 
 export const AddPatientView = () => {
+    const [state, setState] = useState({
+        firstName: "",
+        lastName: "",
+        birthDate: new Date()
+    });
+
+    const [birthDate, setBirthDate] = useState(new Date());
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+
+    const onChangeFirstName = (event) => setFirstName(event.target.value);
+    const onChangeLastName = (event) => setLastName(event.target.value);
+    const changeDate = date => setBirthDate(date);
+
     return (
-        <div>Hello</div>
+        <div>
+            <input
+                name="firstName"
+                value={firstName}
+                onChange={onChangeFirstName}
+            />
+            <input
+                name="lastName"
+                value={lastName}
+                onChange={onChangeLastName}
+            />
+            <DatePicker selected={birthDate} onChange={changeDate} />
+        </div>
     );
 };
